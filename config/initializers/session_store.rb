3b958018dc164fb0b8508c0974bbf11005e8e6da
Adam Duke <adam.v.duke@gmail.com>
2012-11-28 17:41:59 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LikeIt::Application.config.session_store :cookie_store, key: '_like_it_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LikeIt::Application.config.session_store :active_record_store
