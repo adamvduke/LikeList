c3ecda5a90007da57e8fae86e60806967e2ec60b
Adam Duke <adam.v.duke@gmail.com>
2012-12-07 15:35:17 -0500

    make the auth callback work in production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,10 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :instagram, "6143c7e2ac8b4299ac6168a199a6385b", "b1ce8b9ca8a349398d7d4a3faa6b483c"
+
+if Rails.env.production?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :instagram, ENV['IG_CONSUMER_KEY'], ENV['IG_CONSUMER_SECRET']
+  end
+else
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :instagram, "6143c7e2ac8b4299ac6168a199a6385b", "b1ce8b9ca8a349398d7d4a3faa6b483c"
+  end
 end
