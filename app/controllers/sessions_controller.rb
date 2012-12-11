0e4b32d3c0492e564a172205a649100507251296
Adam Duke <adam.v.duke@gmail.com>
2012-12-11 17:19:55 -0500

    only schedule an update, during login, when in production

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     user = User.where(:provider => auth['provider'], :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     user.add_role :admin if User.count == 1 # make the first user an admin
-    user.update_likes
+    user.update_likes if Rails.env.production?
     if user.email.blank?
       redirect_to edit_user_path(user), :alert => "Please enter your email address."
     else
