3aa64f40342806b2632e81e110dea4f5b2a88cd7
Adam Duke <adam.v.duke@gmail.com>
2012-12-16 17:37:13 -0500

    fix post mapper

--- app/models/post_mapper.rb
+++ app/models/post_mapper.rb
@@ -1,21 +1,21 @@
 require 'ostruct'
 
 class PostMapper
-  attr_accessor :post_json
+  attr_accessor :post
   def initialize(post_data)
-    post_json = OpenStruct.new(post_data)
+    @post = OpenStruct.new(post_data)
   end
 
   def attr_hash
     {
-      created_time: post_date.created_time,
+      created_time: post.created_time,
       filter: post.filter,
       ig_id: post.id,
       web_url: post.link,
       username: post.user['username'],
       low_res_image: post.images['low_resolution']['url'],
       standard_res_image: post.images['standard_resolution']['url'],
-      thumbnail: post.images['thumbnail']['url'],
+      thubmbnail: post.images['thumbnail']['url'],
       caption: post.caption.nil? ? '' : post.caption['text']
     }
   end
