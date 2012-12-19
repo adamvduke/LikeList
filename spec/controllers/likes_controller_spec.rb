12a78a7d88a1046c6093dd099ef1862edf46d180
Adam Duke <adam.v.duke@gmail.com>
2012-12-19 14:53:17 -0500

    remove pending specs

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -2,11 +2,4 @@ require 'spec_helper'
 
 describe LikesController do
 
-  describe "GET 'show'" do
-    it "returns http success"
-  end
-  describe "GET 'destroy'" do
-    it "returns http success"
-  end
-
 end
