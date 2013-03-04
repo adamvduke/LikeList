3fe368a122291a16eaa6bc4c405fcdd282824d2d
Adam Duke <adam.v.duke@gmail.com>
2013-03-04 17:48:06 -0500

    make secure_low_res_image and secure_thumbnail work too

--- app/models/like.rb
+++ app/models/like.rb
@@ -8,8 +8,11 @@ class Like < ActiveRecord::Base
     new(like_mapper.attr_hash)
   end
 
-  def secure_standard_res_image
-    standard_res_image.gsub("http", "https")
+  %w[standard_res_image low_res_image thumbnail].each do |method|
+    class_eval <<-RUBY, __FILE__, __LINE__ + 1
+      def secure_#{method}
+        #{method}.gsub("http", "https")
+      end
+    RUBY
   end
-
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,13 +2,27 @@ require 'spec_helper'
 
 describe Like do
 
-  describe "#secure_standard_res_image" do
-    before do
-      @like = FactoryGirl.create(:like, standard_res_image:"http://host.tld/some/path/image.png")
-    end
+  before do
+    @like = FactoryGirl.create(:like)
+  end
 
+  shared_examples_for "a method that returns a secure url" do |method_name|
     it "should return an https url" do
-      @like.secure_standard_res_image.should eq("https://host.tld/some/path/image.png")
+      setter = (method_name.to_s << "=").gsub("secure_", "").to_sym
+      @like.send(setter, "http://host.tld/some/path/image.png")
+      @like.send(method_name).should eq("https://host.tld/some/path/image.png")
     end
+  end
+
+  describe "#secure_standard_res_image" do
+    it_should_behave_like "a method that returns a secure url", :secure_standard_res_image
+  end
+
+  describe "#secure_low_res_image" do
+    it_should_behave_like "a method that returns a secure url", :secure_low_res_image
+  end
+
+  describe "#secure_thumbnail" do
+    it_should_behave_like "a method that returns a secure url", :secure_thumbnail
   end
 end
