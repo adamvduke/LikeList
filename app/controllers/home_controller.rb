99cb3c9170928ac94ca45ca4efbdb7d270ef4584
Adam Duke <adam.v.duke@gmail.com>
2013-08-07 17:55:13 -0400

    only publicly show posts with 'web_url' values

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @likes = Like.order(:created_at).includes(:user).page(params[:page] ||= 1)
+    @likes = Like.where('web_url IS NOT NULL').order(:created_at).includes(:user).page(params[:page] ||= 1)
     if signed_in?
       redirect_to current_user
     else
