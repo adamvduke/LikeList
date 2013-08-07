dcff01274c3431d5cb47d5f79d0e1cac05ba77db
Adam Duke <adam.v.duke@gmail.com>
2013-08-07 18:18:56 -0400

    refactor the like secure url specs

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,27 +2,22 @@ require 'spec_helper'
 
 describe Like do
 
-  before do
-    @like = FactoryGirl.create(:like)
-  end
-
-  shared_examples_for "a method that returns a secure url" do |method_name|
-    it "should return an https url" do
-      setter = (method_name.to_s << "=").gsub("secure_", "").to_sym
-      @like.send(setter, "http://host.tld/some/path/image.png")
-      @like.send(method_name).should eq("https://host.tld/some/path/image.png")
+  describe 'secure urls' do
+    before do
+      url = 'http://host.tld/some/path/image.png'
+      @like = FactoryGirl.create(:like, standard_res_image:url, low_res_image:url, thumbnail:url)
     end
-  end
 
-  describe "#secure_standard_res_image" do
-    it_should_behave_like "a method that returns a secure url", :secure_standard_res_image
-  end
+    shared_examples_for "a method that returns a secure url" do |method_name|
+      it "should return an https url" do
+        @like.send(method_name).should eq("https://host.tld/some/path/image.png")
+      end
+    end
 
-  describe "#secure_low_res_image" do
-    it_should_behave_like "a method that returns a secure url", :secure_low_res_image
-  end
-
-  describe "#secure_thumbnail" do
-    it_should_behave_like "a method that returns a secure url", :secure_thumbnail
+    [:secure_standard_res_image, :secure_low_res_image, :secure_thumbnail].each do |method|
+      describe "##{method}" do
+        it_should_behave_like "a method that returns a secure url", method
+      end
+    end
   end
 end
