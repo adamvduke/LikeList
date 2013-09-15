2e488f7e9252c2b2441ba512ceacd75ceac0311a
Adam Duke <adam.v.duke@gmail.com>
2013-09-15 09:52:43 -0400

    add omniauth full host

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :instagram, ENV['IG_CONSUMER_KEY'], ENV['IG_CONSUMER_SECRET']
 end
+
+if Rails.env.production?
+  OmniAuth.config.full_host = 'http://www.likelist.me'
+end
