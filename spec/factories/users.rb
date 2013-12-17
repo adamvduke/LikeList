672dc893063cb9531b9eda1b32bb0edc8c6c0d25
Adam Duke <adam.v.duke@gmail.com>
2013-12-17 03:52:23 -0500

    make factoried users unique

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,11 +5,16 @@ FactoryGirl.define do
     ((Random.rand(100000) * Random.rand(100000)) / n ).to_s
   end
 
+  sequence :token do |n|
+    "#{FactoryGirl.generate(:uid)}.#{FactoryGirl.generate(:uid)}"
+  end
+
   factory :user do
-    name Faker::Name.name
-    email Faker::Internet.email
-    nickname Faker::Internet.user_name
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    nickname { Faker::Internet.user_name }
     provider "instagram"
     uid
+    token { FactoryGirl.generate(:token) }
   end
 end
