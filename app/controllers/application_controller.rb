a498f2da22b2bf9d108a0c72280fcbb880d2bd42
Adam Duke <adam.v.duke@gmail.com>
2014-11-01 16:56:38 -0400

    set a max per_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   include ActsAsTaggableOn::TagsHelper
 
   def paginate(likes)
-    Like.per_page = params[:per_page] ||= 5
+    per_page = params[:per_page] ||= 5
+    per_page = 20 if per_page.to_i > 20
+    Like.per_page = per_page
     likes.includes(:user).order("created_time DESC").page(params[:page])
   end
 end
