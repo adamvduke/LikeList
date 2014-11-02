52199a90fc51e057d966157c95bb997ac7a1dfa9
Adam Duke <adam.v.duke@gmail.com>
2014-11-01 23:41:36 -0400

    only fetch random likes if no one is signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @likes = Like.where('web_url IS NOT NULL').order(:created_at).includes(:user).page(params[:page] ||= 1)
     if signed_in?
       redirect_to current_user
     else
+      @likes = Like.where('web_url IS NOT NULL').order(:created_at).includes(:user).page(params[:page] ||= 1)
       render :index
     end
   end
