f712a878992ece16eeb9e51432919ca2a42482f0
Adam Duke <adam.v.duke@gmail.com>
2014-11-04 23:09:38 -0500

    add some spaces in places

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,12 @@ LikeList::Application.routes.draw do
   resources :users, only: [ :index, :show, :edit, :update, :destroy ] do
     resources :likes, only: [ :update, :destroy ]
   end
-  post '/likes/broken' => 'likes#report_broken', as: :report_broken_like
-  get '/auth/:provider/callback' => 'sessions#create'
-  get '/signin' => 'sessions#new', as: :signin
-  get '/signout' => 'sessions#destroy', as: :signout
-  get '/auth/failure' => 'sessions#failure'
-
-  get   '/download_likes_script' => 'download#likes_script'
+  post '/likes/broken' =>            'likes#report_broken', as: :report_broken_like
+  get  '/auth/:provider/callback' => 'sessions#create'
+  get  '/signin' =>                  'sessions#new',        as: :signin
+  get  '/signout' =>                 'sessions#destroy',    as: :signout
+  get  '/auth/failure' =>            'sessions#failure'
+  get  '/download_likes_script' =>   'download#likes_script'
 
   mount Sidekiq::Web => '/sidekiq',  constraints: AdminConstraint.new
 end
