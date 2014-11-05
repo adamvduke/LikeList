e03725e1d0882da3ba245d580761dc63aad23417
Adam Duke <adam.v.duke@gmail.com>
2014-11-04 22:14:04 -0500

    move secret key base to a config file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-LikeList::Application.config.secret_key_base = '8517b3d4be58c945607b91af935a20e71ff8dc4aff6f9e2f1d24a9b3dbe707381686c53fbcf1bc08313ed439b3c8ddaa6e54bd18297b8529dec497cf9d4e702d'
+LikeList::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
