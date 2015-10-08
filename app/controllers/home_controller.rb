c08a8bd7d25a6a9aa68661887eb8619e4e5d1ffb
Adam Duke <adam.v.duke@gmail.com>
2015-10-07 23:01:42 -0400

    don't bother looking for web url not null because it's slow

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     if signed_in?
       redirect_to current_user
     else
-      @likes = Like.where('web_url IS NOT NULL').order(:created_at).includes(:user).page(params[:page] ||= 1)
+      @likes = Like.order(:created_at).includes(:user).page(params[:page] ||= 1)
       render :index
     end
   end
