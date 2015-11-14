fcf9f151ead84842dfff697676712801af3d7093
Adam Duke <adam.v.duke@gmail.com>
2015-11-14 18:48:30 -0500

    delete likes that are unavailable

--- app/workers/like_verification_worker.rb
+++ app/workers/like_verification_worker.rb
@@ -5,7 +5,7 @@ class LikeVerificationWorker
     begin
       Rails.logger.info "Verifying like #{like_id}"
       RestClient.head(image_url)
-    rescue RestClient::Forbidden, RestClient::ResourceNotFound => ex
+    rescue RestClient::Forbidden, RestClient::ResourceNotFound, RestClient::ServiceUnavailable => ex
       Rails.logger.info "Like: #{like_id} caused a #{ex.class} exception"
       Rails.logger.info "URL: #{image_url}"
       Like.find(like_id).destroy
