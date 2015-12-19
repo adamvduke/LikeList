1c74cefbc56b00702531b66158b0c45c46b2bd76
Adam Duke <adam.v.duke@gmail.com>
2015-12-18 20:54:47 -0500

    update sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,3 @@
-# load celluloid before DynamicFetch to avoid uninitialized constant
-# Celluloid::Actor error
-require 'celluloid'
-
 Sidekiq.configure_server do |config|
   config.redis = {url: ENV['REDIS_PROVIDER'], namespace: "sidekiq-#{Rails.env}"}
 end
@@ -9,15 +5,3 @@ end
 Sidekiq.configure_client do |config|
   config.redis = {url: ENV['REDIS_PROVIDER'], namespace: "sidekiq-#{Rails.env}"}
 end
-
-##
-# recommended by https://github.com/celluloid/celluloid/wiki/Logging
-#
-Celluloid.logger = Rails.logger
-
-if Rails.env.test?
-  Sidekiq.logger.level = Logger::ERROR
-else
-  Sidekiq.logger.level = Logger::WARN
-end
-
